01a7ce2968e65fc6e4ca2aa4efcf6cf0e3fb7eba
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-10-16 16:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 // You can modify this code
 // Product data
 const products = [
-    { "id": 1, "name": "Product 1", "price": 10 },
-    { "id": 2, "name": "Product 2", "price": 20 },
-    { "id": 3, "name": "Product 3", "price": 30 },
-    { "id": 4, "name": "Product 4", "price": 40 },
-    { "id": 5, "name": "Product 5","price": 50 },
+    { id: 1, name: "Product 1", price: 10 },
+    { id: 2, name: "Product 2", price: 20 },
+    { id: 3, name: "Product 3", price: 30 },
+    { id: 4, name: "Product 4", price: 40 },
+    { id: 5, name: "Product 5", price: 50 },
 ];
 
 const productList = document.getElementById("product-list");
@@ -40,6 +40,7 @@ function addToCart(productId) {
 
     if (productToAdd) {
         let cartItems = JSON.parse(sessionStorage.getItem("cart")) || [];
+        // Prevent adding duplicate products
         const existingProduct = cartItems.find(item => item.id === productToAdd.id);
         if (!existingProduct) {
             cartItems.push(productToAdd);
